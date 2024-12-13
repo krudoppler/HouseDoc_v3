c8c85c391944185f6ca827b11ae243a8c09847d8
krudoppler <bkru33@gmail.com>
2024-12-13 17:13:49 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+export const homeData = [
+    {
+      homeUtilities: ["HVAC", "Plumbing", "Electrical"],
+    },
+    {
+    homeUtilities: ["Roof", "Bathroom", "Poop"],
+    },
+    {
+    homeUtilities: ["Thermos", "Bathroom", "Pee"],
+    },
+]
